a9e9034e82c245e0a1c68732703f2d8db8de49dc
Oleksandr Kokhno <kohnoan@gmail.com>
2023-06-16 18:53:44 +0300

    copy

---
+++ src/components/App/App.jsx
@@ -0,0 +1,18 @@
+import css from './app.module.css';
+import ContactForm from 'components/ContactForm';
+import ContactList from 'components/ContactList';
+import Filter from 'components/Filter';
+
+const App = () => {
+  return (
+    <div className={css.container}>
+      <h1>Phonebook</h1>
+      <ContactForm />
+      <h2>Contacts</h2>
+      <Filter />
+      <ContactList />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,33 @@
+import css from './contactList.module.css';
+import { useSelector, useDispatch } from 'react-redux';
+import { deleteContact } from 'redux/contacts/contacts-slice';
+import { getFilteredContacts } from 'redux/contacts/contacts-selectors';
+
+const ContactList = () => {
+  const dispatch = useDispatch();
+  const contacts = useSelector(getFilteredContacts);
+
+  const onDeleteContact = id => {
+    dispatch(deleteContact(id));
+  };
+
+  return (
+    <ul className={css.contactList}>
+      {contacts.map(contact => (
+        <li className={css.contactItem} key={contact.id}>
+          <span>{contact.name}: </span>
+          <span>{contact.number}</span>
+          <button
+            className={css.deleteButton}
+            type="button"
+            onClick={() => onDeleteContact(contact.id)}
+          >
+            Delete
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default ContactList;
